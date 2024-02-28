f62d3de4da34f576071d152cc6362fa3ddd8a68f
Paulo Ramirez <pa.ramirezciani13@gmail.com>
2024-02-28 17:58:40 -0300

    rehacer

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # cafeteria
 Proyecto para pyme 
+
+PALETA DE COLORES 
+paleta de colores a utilizar:
+#0F7D7E
+Copiar
+#76B5A0
+Copiar
+#FFFDD1
+Copiar
+#FF7575
+Copiar
+#D33649
--- main.js
+++ main.js
@@ -1,33 +1,9 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const detailsButtons = document.querySelectorAll('.details-btn');
-    const productDetails = document.querySelector('.product-details');
-    const closeBtn = document.querySelector('.close-btn');
-
-    // Mostrar detalles del producto al hacer clic en el botón "Ver detalles"
-    detailsButtons.forEach(button => {
-        button.addEventListener('click', function() {
-            const productId = this.getAttribute('data-product');
-            const productDetailsContent = document.querySelector('.product-details .details-content');
-
-            // Aquí puedes cargar los detalles del producto desde una base de datos o alguna fuente de datos
-            // Por ahora, simplemente mostramos un texto de ejemplo
-            productDetailsContent.innerHTML = `
-                <h2>Detalles del Producto ${productId}</h2>
-                <div class="ingredients">
-                    <h3>Ingredientes:</h3>
-                    <p>Lista de ingredientes del producto ${productId}.</p>
-                </div>
-                <button class="close-btn">Cerrar</button>
-            `;
-
-            productDetails.style.display = 'flex';
-
-            // Escuchar eventos del botón "Cerrar" dentro de los detalles del producto
-            const closeDetailsBtn = document.querySelector('.product-details .close-btn');
-            closeDetailsBtn.addEventListener('click', function() {
-                productDetails.style.display = 'none';
-            });
-        });
+    const menuToggle = document.querySelector('.menu-toggle');
+    const navLinks = document.querySelector('.nav-links');
+  
+    menuToggle.addEventListener('click', function() {
+      navLinks.classList.toggle('show');
     });
-
-});
+  });
+  
