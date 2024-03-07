8fc816e3aa984bc99bf752e50845683e95665592
Paulo Ramirez <pa.ramirezciani13@gmail.com>
2024-03-07 11:48:56 -0300

    finish

--- main.js
+++ main.js
@@ -1,9 +1,21 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const menuToggle = document.querySelector('.menu-toggle');
-    const navLinks = document.querySelector('.nav-links');
+  // Función para mostrar el popup con la descripción del producto
+  function mostrarPopup(titulo, descripcion) {
+    document.getElementById("popup-titulo").innerText = titulo;
+    document.getElementById("popup-descripcion").innerText = descripcion;
+    document.getElementById("popup").style.display = "block";
+}
+
+// Función para cerrar el popup
+function cerrarPopup() {
+    document.getElementById("popup").style.display = "none";
+}
   
-    menuToggle.addEventListener('click', function() {
-      navLinks.classList.toggle('show');
-    });
-  });
-  
+
+window.addEventListener("scroll", function() {
+  var button = document.getElementById("whatsapp-button");
+  if (window.pageYOffset > 100) { // Cambia 100 según sea necesario para ajustar el punto de activación
+      button.style.transform = "translateX(-60px)"; // Mueve el botón hacia la izquierda
+  } else {
+      button.style.transform = "translateX(0)"; // Vuelve a la posición original
+  }
+});
